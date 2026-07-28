878b540b795e4c8b095cb142a59b9c405e147c0a
Andrei T. <138087405+2004drei@users.noreply.github.com>
2026-07-28 13:46:09 +0800

    Triangle Quests

---
+++ Python/math/triangle-quest.py
@@ -0,0 +1,8 @@
+# print(*[10**(i)//9*i for i in range(1, int(input()))])
+
+
+for i in range(1, int(input())): print((10**i // 9) * i)
+
+
+# for i in range(1, int(input())):
+#   print((10**(i)//9)*i)
